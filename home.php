41583b88099444c9835e95aa5f274b2b4cf4c01e
Alain SINNAPPAN <Alain.Sinnappan@outlook.com>
2023-01-25 16:08:41 +0100

    25/01

--- home.php
+++ home.php
@@ -1,55 +1,156 @@
-<section class="ftco-section">
-<div class="container">
-<div class="row justify-content-center">
-<div class="col-md-6 text-center mb-5">
-<h2 class="heading-section">Login #05</h2>
-</div>
-</div>
-<div class="row justify-content-center">
-<div class="col-md-7 col-lg-5">
-<div class="wrap">
-<div class="img" style="background-image: url(images/bg-1.jpg);"></div>
-<div class="login-wrap p-4 p-md-5">
-<div class="d-flex">
-<div class="w-100">
-<h3 class="mb-4">Sign In</h3>
-</div>
-<div class="w-100">
-<p class="social-media d-flex justify-content-end">
-<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
-<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
-</p>
-</div>
-</div>
-<form action="#" class="signin-form">
-<div class="form-group mt-3">
-<input type="text" class="form-control" required>
-<label class="form-control-placeholder" for="username">Username</label>
-</div>
-<div class="form-group">
-<input id="password-field" type="password" class="form-control" required>
-<label class="form-control-placeholder" for="password">Password</label>
-<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-</div>
-<div class="form-group">
-<button type="submit" class="form-control btn btn-primary rounded submit px-3">Sign In</button>
-</div>
-<div class="form-group d-md-flex">
-<div class="w-50 text-left">
-<label class="checkbox-wrap checkbox-primary mb-0">Remember Me
-<input type="checkbox" checked>
-<span class="checkmark"></span>
-</label>
-</div>
-<div class="w-50 text-md-right">
-<a href="#">Forgot Password</a>
-</div>
-</div>
-</form>
-<p class="text-center">Not a member? <a data-toggle="tab" href="#signup">Sign Up</a></p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</section>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Google Calendar API Quickstart</title>
+    <meta charset="utf-8" />
+  </head>
+  <body>
+    <p>Google Calendar API Quickstart</p>
+
+    <!--Add buttons to initiate auth sequence and sign out-->
+    <button id="authorize_button" onclick="handleAuthClick()">Authorize</button>
+    <button id="signout_button" onclick="handleSignoutClick()">Sign Out</button>
+
+    <pre id="content" style="white-space: pre-wrap;"></pre>
+
+    <script type="text/javascript">
+      /* exported gapiLoaded */
+      /* exported gisLoaded */
+      /* exported handleAuthClick */
+      /* exported handleSignoutClick */
+
+      // TODO(developer): Set to client ID and API key from the Developer Console
+      const CLIENT_ID = '872037188017-csbrj5todvu8mauru5tvbb4s1g9hgvg7.apps.googleusercontent.com';
+      const API_KEY = '<AIzaSyAd6aXlXO2Y-6nVGRVeNsxWtD0T_oC2_10>';
+
+      // Discovery doc URL for APIs used by the quickstart
+      const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+
+      let tokenClient;
+      let gapiInited = false;
+      let gisInited = false;
+
+      document.getElementById('authorize_button').style.visibility = 'hidden';
+      document.getElementById('signout_button').style.visibility = 'hidden';
+
+      /**
+       * Callback after api.js is loaded.
+       */
+      function gapiLoaded() {
+        gapi.load('client', initializeGapiClient);
+      }
+
+      /**
+       * Callback after the API client is loaded. Loads the
+       * discovery doc to initialize the API.
+       */
+      async function initializeGapiClient() {
+        await gapi.client.init({
+          apiKey: API_KEY,
+          discoveryDocs: [DISCOVERY_DOC],
+        });
+        gapiInited = true;
+        maybeEnableButtons();
+      }
+
+      /**
+       * Callback after Google Identity Services are loaded.
+       */
+      function gisLoaded() {
+        tokenClient = google.accounts.oauth2.initTokenClient({
+          client_id: CLIENT_ID,
+          scope: SCOPES,
+          callback: '', // defined later
+        });
+        gisInited = true;
+        maybeEnableButtons();
+      }
+
+      /**
+       * Enables user interaction after all libraries are loaded.
+       */
+      function maybeEnableButtons() {
+        if (gapiInited && gisInited) {
+          document.getElementById('authorize_button').style.visibility = 'visible';
+        }
+      }
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick() {
+        tokenClient.callback = async (resp) => {
+          if (resp.error !== undefined) {
+            throw (resp);
+          }
+          document.getElementById('signout_button').style.visibility = 'visible';
+          document.getElementById('authorize_button').innerText = 'Refresh';
+          await listUpcomingEvents();
+        };
+
+        if (gapi.client.getToken() === null) {
+          // Prompt the user to select a Google Account and ask for consent to share their data
+          // when establishing a new session.
+          tokenClient.requestAccessToken({prompt: 'consent'});
+        } else {
+          // Skip display of account chooser and consent dialog for an existing session.
+          tokenClient.requestAccessToken({prompt: ''});
+        }
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick() {
+        const token = gapi.client.getToken();
+        if (token !== null) {
+          google.accounts.oauth2.revoke(token.access_token);
+          gapi.client.setToken('');
+          document.getElementById('content').innerText = '';
+          document.getElementById('authorize_button').innerText = 'Authorize';
+          document.getElementById('signout_button').style.visibility = 'hidden';
+        }
+      }
+
+      /**
+       * Print the summary and start datetime/date of the next ten events in
+       * the authorized user's calendar. If no events are found an
+       * appropriate message is printed.
+       */
+      async function listUpcomingEvents() {
+        let response;
+        try {
+          const request = {
+            'calendarId': 'primary',
+            'timeMin': (new Date()).toISOString(),
+            'showDeleted': false,
+            'singleEvents': true,
+            'maxResults': 10,
+            'orderBy': 'startTime',
+          };
+          response = await gapi.client.calendar.events.list(request);
+        } catch (err) {
+          document.getElementById('content').innerText = err.message;
+          return;
+        }
+
+        const events = response.result.items;
+        if (!events || events.length == 0) {
+          document.getElementById('content').innerText = 'No events found.';
+          return;
+        }
+        // Flatten to string to display
+        const output = events.reduce(
+            (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
+            'Events:\n');
+        document.getElementById('content').innerText = output;
+      }
+    </script>
+    <script async defer src="https://apis.google.com/js/api.js" onload="gapiLoaded()"></script>
+    <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
+  </body>
+</html>
