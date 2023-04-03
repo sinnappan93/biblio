f4d88900092721db64d749e198c313a537293a16
Alain SINNAPPAN <Alain.Sinnappan@outlook.com>
2023-04-03 02:31:56 +0200

    home

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -1,6 +1,6 @@
 <h1>page admin</h1>
 
-<a href="?page=test"><button type="button" class="btn btn-sucess btn-sm">Calendrier test</button></a>
+<a href="?page=test"><button type="button" class="btn btn-success btn-sm">Calendrier test</button></a>
 
 <div id="calendrier"></div>
 
