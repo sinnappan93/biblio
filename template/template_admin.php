894f02d870e68c37f3638935c27854a8e37521a2
Alain SINNAPPAN <Alain.Sinnappan@outlook.com>
2023-05-28 02:28:44 +0200

    modification script.js pour quil y ai 2 version differentes

--- template/template_admin.php
+++ template/template_admin.php
@@ -18,7 +18,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
-    <script src="javascript/script.js"></script>
+    <script src="javascript/script_admin.js"></script>
     <script>
         var scheds = $.parseJSON('<?= json_encode($sched_res) ?>')
     </script>
